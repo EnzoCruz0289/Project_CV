3152f7ba4be7fc7c68af177400f6b34620e27eb9
EnzoCruz0289 <josedavidaragoncruz@gmail.com>
2025-05-30 15:55:27 -0500

    AJUSTE DE TEXTO Y SCROLLBAR, INICIO DE PAGINA CV

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CvComponent } from './pages/cv/cv.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { ProjectsComponent } from './pages/projects_jdac/projects.component';
+import { PersonalinformationComponent } from './pages/personalinformation/personalinformation.component';
 
 export const routes: Routes = [
     {path: 'cv', component: CvComponent},
     {path: 'inicio', component: InicioComponent},
     {path: 'proyectos', component: ProjectsComponent},
+    {path: 'personalinformation', component:PersonalinformationComponent}
 ];
